46be8a3b42f041688b5c9cfd840f40c58fe80b69
ttian99 <176553611@qq.com>
2016-09-27 05:13:12 +0800

    增加忽略文件

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ function sendMsg(socket) {
 	socket.on('message', function(obj) {
 		//向所有客户端广播发布的消息
 		io.emit('message', obj);
-		console.log(obj.username + '说：' + obj.content);
+		console.log(obj.username + '说：' + obj.msg);
 	});
 }
